4163607e40f68f5d70914fcb1206536699701c99
Sezin Çağla Serficeli <serficelis@gmail.com>
2024-08-23 20:29:31 +0300

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+export default function Footer(props) {
+    const{handleToggleModal, data} = props
+
+  return (
+    <footer>
+        <div className="bgGradient"></div>
+        <div>
+            <h2>{data?.title}</h2>
+            <h1>APOD PROJECT</h1>
+        </div>
+        <button onClick={handleToggleModal}>
+            <i className="fa-solid fa-circle-info"></i>
+        </button>
+    </footer>
+  )
+}
